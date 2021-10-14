ecb5d3c020dc9bfacf25866fa30ec8ae267629d5
Carla Marzin <carla.m-@hotmail.fr>
2021-10-14 10:57:36 +0200

    Test 5

--- js/index.js
+++ js/index.js
@@ -1,15 +1,6 @@
    "use strict";
-  
-
-  //DISPLAY THE JSON DATA 
-  //Fill the JSON data into the div id photographers dynamically 
-  //Loop through every object in our JSON object
-
 
   fetchData("fishEyeData.json").then((data)=>{
-
-       //récupérer la div, intégrer le html
-      //  document.querySelector("#photographers").innerHTML = createPhotographers(data);
   });
 
   
