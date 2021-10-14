397e642cfbbd44b008846925c39f9612b3bd957a
Carla Marzin <carla.m-@hotmail.fr>
2021-10-14 09:20:17 +0200

    Fetch Json data + essay filtrer par tag

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-//Fetch JSON data 
+//FETCH JSON DATA 
 
-fetch('./photographers.json')
+fetch('photographers.json')
   .then(function (response) {
     return response.json();
    // The JSON data will arrive here as a response
   })
   .then(function (data) {
     appendData(data);
-   //we get the actual JSON data as a parameter. This data looks just like the data in our JSON file. 
+   //récupérer la donnée lisible  
    //This is where we create the code which will append the data to our page.
   })
   .catch(function (err) {
@@ -15,8 +15,9 @@ fetch('./photographers.json')
     // If an error occured, you will catch it here
   });
 
+  
+  
   //DISPLAY THE JSON DATA 
-
   //Fill the JSON data into the div id photographers dynamically 
   //Loop through every object in our JSON object
 
@@ -48,3 +49,41 @@ fetch('./photographers.json')
 
   //récupérer la div, intégrer le html
   document.querySelector("#photographers").innerHTML = html;
+
+
+
+//SELECTION PAR LES TAGS 
+
+//je store les tags dans une variable
+var portraits = document.getElementsByClassName(portraits);
+var art = document.getElementsByClassName(art);
+var fashion = document.getElementsByClassName(fashion); 
+var architecture = document.getElementsByClassName(architecture);
+var travel = document.getElementsByClassName(travel);
+var sport = document.getElementsByClassName(sport);
+var animals = document.getElementsByClassName(animals);
+var events = document.getElementsByClassName(events);
+
+var photographerTag = data.tag;
+
+//au click sur le tag, j'affiche les photographes selectionnés
+const tag = document.getElementsByClassName(tag);
+//const filteredTag = tag.checked >>>>> faire un addEventListenner sur le click du tag
+// const tag = ['portraits', 'art', 'fashion', 'architecture', 'travel', 'sport', 'animals', 'events']
+// const filtered = photographers.filter( function(photographer){
+//   return photographer.tag === tagChecked;})
+//console.log(filtered)
+tag.addEventListener("click", tagFiltering);
+
+//Si le photographe ne contient pas le tag choisis
+function tagFiltering(e){
+  //si le tag est checké, alors je veux que les photographes qui ont ce tag, s'affichent
+  if (portrait.checked){
+    tag.className = "";
+    return true;
+  //sinon, je ne veux pas que le photographe s'affiche, je lui ajoute la className "invisible", avec un display none en css
+  }else{
+    tag.className = "invisible";
+    return false;
+  }
+}
