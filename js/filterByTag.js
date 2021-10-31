96f825ccccf73f1c0f3a0e58a2a1b2ce747b1918
Carla Marzin <carla.m-@hotmail.fr>
2021-10-31 14:31:23 +0100

    Validation du formulaire

--- js/filterByTag.js
+++ js/filterByTag.js
@@ -9,10 +9,13 @@
  */  
 
  function filterByTag(photographerArr, tag){
+     // je crée une fonction à deux paramètres : l'array des tags de photographes, et le tag selectionné par le user 
 
       const result = photographerArr.filter((photograph)=>{
 
            //photograph?.tags est le tableau qui contient les differents tags d'un photographe
+           // si celui-ci contient le tag selectionné, alors je retourne le photographe
+           //ce résultat est storé dans la constante result
 
            if(photograph?.tags.includes(tag) === true){
 
@@ -22,6 +25,7 @@
 
       });
 
+      //cette fonction renvoie la const result (html des photogarphes)
       return result;
 
 
--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
   "use strict";
 
+// La méthode then renvoie un objet promise, elle prend un argument callback à utiliser en cas d'échec de la promise
+
   fetchData("../json/fishEyeData.json").then((data)=>{
 
       if(data === "Impossible de recupérer les données"){
@@ -8,25 +10,32 @@
 
       }
 
+      // je récupère l'endroit dans le DOM, où intégrer les photographes 
+      // je lui applique la fonction createPhotographers, pour lui intégrer le contenu, à partir des data 
+
       document.querySelector("#photographers").innerHTML = createPhotographers(data?.photographers);
 
       const tags = document.querySelectorAll('.buttonTag');
+      // je récupère dans le DOM, l'emplacement des tags à selectionner
 
       const j = tags.length;
 
       for(let i=0; i<j; i++){
+        // je loop au travers chaque tags 
 
            tags[i].addEventListener("click", function(){
-
-
+                // J'écoute l'évènement click sur chaque tag
                 // On recupere la valeur textuelle du tag
                 // https://developer.mozilla.org/fr/docs/Web/HTML/Global_attributes/data-*
 
                 const actualTag = this.dataset?.tag;
+                // Je récupère dans le DOM le tag selectionné , et récupère son nom exact, grace au data-tag
 
                 const filterPhotographers = filterByTag(data?.photographers, actualTag);
+                // J'applique ensuite la fonction pour filtrer les photographes selon leurs tags
 
                 document.querySelector("#photographers").innerHTML = createPhotographers(filterPhotographers);
+                // J'ajoute le HTML des photographes correspondants au tag
 
 
            });
